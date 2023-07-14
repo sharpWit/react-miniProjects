5c2905ce8154e10e53918775d44cd35f9b6d0908
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-15 00:36:15 +0330

    change the directory

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import FlashCards from "./components/steps/flashcards/FlashCards";
+import FlashCards from "./components/flashcards/FlashCards";
 // import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
