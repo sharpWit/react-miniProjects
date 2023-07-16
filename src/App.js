38cb3cba4e86a2a8bc3e7792e5ada514319d7ae3
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-16 15:13:47 +0330

    complete packing list

--- src/App.js
+++ src/App.js
@@ -1,90 +1,52 @@
 import { useState } from "react";
+import Stats from "./components/Stats";
+import PackingList from "./components/PackingList";
+import Form from "./components/Form";
+import Logo from "./components/Logo";
 
-const initialItems = [
-  { id: 1, description: " Passports", quantity: 2, packed: false },
-  { id: 2, description: " Socks", quantity: 12, packed: true },
-  { id: 3, description: " charger", quantity: 1, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "charger", quantity: 1, packed: false },
+// ];
 
 export default function App() {
-  return (
-    <div className="app">
-      <Logo />
-      <Form />
-      <PackingList />
-      <Stats />
-    </div>
-  );
-}
-function Logo() {
-  return <h1>🌴 Far Away 💼</h1>;
-}
-function Form() {
-  const [description, setDescription] = useState("");
-  const [quantity, setQuantity] = useState(0);
+  const [items, setItems] = useState([]);
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
-  function handleSubmit(e) {
-    e.preventDefault();
+  function handleDeleteItems(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
 
-    if (!description) return;
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
 
-    const newItem = { description, quantity, packed: false, id: Date.now() };
-    console.log(newItem);
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you sure you want to delete all items?"
+    );
 
-    setDescription("");
-    setQuantity(1);
+    if (confirmed) setItems([]);
   }
 
   return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your 😍 trip?</h3>
-      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
-        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}
-      </select>
-      <input
-        type="text"
-        placeholder="Item..."
-        value={description}
-        onChange={(e) => {
-          setDescription(e.target.value);
-        }}
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItems}
+        onToggleItem={handleToggleItem}
+        onClearList={handleClearList}
       />
-      <button>Add</button>
-    </form>
-  );
-}
-function PackingList() {
-  return (
-    <div className="list">
-      <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
-        ))}
-      </ul>
+      <Stats items={items} />
     </div>
-  );
-}
-
-function Item({ item }) {
-  return (
-    <li>
-      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
-        {item.quantity}
-        {item.description}
-      </span>
-      <button>❌</button>
-    </li>
-  );
-}
-
-function Stats() {
-  return (
-    <footer className="stats">
-      <em>💼 You have X items on your list, and you already packed X (X%)</em>
-    </footer>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import FlashCards from "./components/flashcards/FlashCards";
-// import App from "./App";
+// import FlashCards from "./components/flashcards/FlashCards";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <FlashCards />
+    <App />
+    {/* <FlashCards /> */}
   </React.StrictMode>
 );
