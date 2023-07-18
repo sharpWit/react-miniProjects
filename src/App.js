d57d5fd308513ce90f309e5feb974540275ad95d
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-19 00:37:08 +0330

    rating completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 // import UsePopcorn from "./components/usepopcorn/UsePopcorn";
-import StarRating from "./components/usepopcorn/StarRating";
+import StarRating from "./components/rating/StarRating";
 
 function App() {
   return (
     <>
-      <StarRating maxRating={5} />
+      <StarRating
+        maxRating={5}
+        messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      />
+      <StarRating size={24} color="red" className="test" defaultValue={1} />
       {/* <UsePopcorn /> */}
     </>
   );
---
+++ src/components/rating/StarRating.js
@@ -0,0 +1,69 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Star from "./Star";
+
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+const starContainerStyle = {
+  display: "flex",
+};
+
+StarRating.propTypes = {
+  maxRating: PropTypes.number,
+  defaultValue: PropTypes.number,
+  color: PropTypes.string,
+  size: PropTypes.number,
+  messages: PropTypes.array,
+  className: PropTypes.string,
+  onSetRating: PropTypes.func,
+};
+
+function StarRating({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 48,
+  className,
+  messages = [],
+  defaultValue = 0,
+}) {
+  const [rating, setRating] = useState(defaultValue);
+  const [tempRating, setTempRating] = useState(0);
+
+  function handleRating(rating) {
+    setRating(rating);
+  }
+
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color,
+    fontSize: `${size / 1.5}px`,
+  };
+  return (
+    <div style={containerStyle} className={className}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <Star
+            key={i}
+            onRate={() => handleRating(i + 1)}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
+            color={color}
+            size={size}
+          />
+        ))}
+      </div>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
+    </div>
+  );
+}
+
+export default StarRating;
