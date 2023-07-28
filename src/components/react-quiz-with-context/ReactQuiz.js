eb56d516ab716cb35af36d0707c43f9b9453bc8f
Saeed Khosravi <91289174+sharpWit@users.noreply.github.com>
2023-07-28 20:32:03 +0330

    quiz app updated with using of context

---
+++ src/components/react-quiz-with-context/ReactQuiz.js
@@ -0,0 +1,39 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Loader from "./components/Loader";
+import Error from "./components/Error";
+import StartScreen from "./components/StartScreen";
+import Question from "./components/Question";
+import NextButton from "./components/NextButton";
+import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
+import Footer from "./components/Footer";
+import Timer from "./components/Timer";
+import { useQuiz } from "./contexts/QuizContext";
+
+export default function App() {
+  const { status } = useQuiz();
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen />}
+        {status === "active" && (
+          <>
+            <Progress />
+            <Question />
+            <Footer>
+              <Timer />
+              <NextButton />
+            </Footer>
+          </>
+        )}
+        {status === "finished" && <FinishScreen />}
+      </Main>
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import ReactQuiz from "./components/react-quiz-with-context/ReactQuiz";
+import "./components/react-quiz-with-context/index.css";
+import { QuizProvider } from "./components/react-quiz-with-context/contexts/QuizContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <QuizProvider>
+      <ReactQuiz />
+    </QuizProvider>
   </React.StrictMode>
 );
